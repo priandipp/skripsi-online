657cc57c4aadd9197c382727f5a5d6351f663317
Daniel Pardamean <danielpardamean.dev@gmail.com>
2018-02-21 21:22:16 +0700

    add allMahasiswa query

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,7 @@
 export default {
   Query: {
-    hi: (parent, args, context, info) => 'hi'
+    hi: (parent, args, context, info) => 'hi',
+    allMahasiswa: (parent, args, { models }, info) => models.Mahasiswa.findAll()
   },
   Mutation: {
     createMahasiswa: (parent, args, { models }) => models.Mahasiswa.create(args)
--- schema.js
+++ schema.js
@@ -13,5 +13,6 @@ export default `
 
     type Query {
         hi: String
+        allMahasiswa: [Mahasiswa]
     }
 `;
